6bded02f13f64989832c970dd6d484482389e48a
fmarinosticketcalender <yokohamafmarinos.ticketcalender@gmail.com>
2025-05-30 23:04:22 +0900

    Update puppeteer_scraper.js

--- puppeteer_scraper.js
+++ puppeteer_scraper.js
@@ -2,8 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: 'new',
+  const browser = await puppeteer.launch({ headless: 'new',
     args: ['--no-sandbox', '--disable-setuid-sandbox']  // ここがポイント
   });
 
